d15a8efa3b658bffe75db3ca7694d354c62dc8b6
Francis-Mwaniki <francismwaniki630@gmail.com>
2024-05-10 13:36:23 +0300

    added aptible config

--- order_mgt_sys/settings.py
+++ order_mgt_sys/settings.py
@@ -19,11 +19,11 @@ SECRET_KEY = "auth0-order_mgt_sys-k0n4a#6cqu9=co$_bu^^sd@&^8#*%ukg3z4ku!lj&j)%^@
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS =os.getenv('ALLOWED_HOSTS').split(' ')
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
